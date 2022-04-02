ad5898c6ab61a7c578910df57deab8679e52d5af
Dame <dcampbell7095@gmail.com>
2022-04-01 21:29:11 -0400

    adding Chat props

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import './App.css';
+
+
+const App = () => {
+    return (
+    <ChatEngine 
+            height = "100vh"
+            projectID = "466aae7b-7045-4d6c-bd01-fee2b745bd0c"
+            userName= "dcam"
+            userSecret= "123123"
+            
+            />        
+    );
+};
+
+export default App;
